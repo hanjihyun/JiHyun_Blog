4f0ec8bf05baf68ea1b5f31838398707ac6c9f01
한지현 <gksgpals96@naver.com>
2021-12-22 20:57:05 +0900

    First Commit

---
+++ src/test/java/com/springboot/jihyun_blog/JihyunBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jihyun_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JihyunBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
