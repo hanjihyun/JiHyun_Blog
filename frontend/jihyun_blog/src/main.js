13067d89fcbe346d8921d54e49b286869cb69ecc
한지현 <gksgpals96@naver.com>
2022-01-04 01:42:57 +0900

    Add : Vue js Platform

--- frontend/jihyun_blog/src/main.js
+++ frontend/jihyun_blog/src/main.js
@@ -1,9 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './plugins/element.js'
+Vue.use(BootstrapVue)
+import MaterialKit from "./plugins/material-kit";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(MaterialKit);
+
+const NavbarStore = {
+  showNavbar: false
+};
+
+Vue.mixin({
+  data() {
+    return {
+      NavbarStore
+    };
+  }
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
+
