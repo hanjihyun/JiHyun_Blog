6a9ec709d9d202368a3116d268aaab58f9cb3f29
한지현 <gksgpals96@naver.com>
2022-01-23 21:23:51 +0900

    Fix : setting

--- backend/src/main/java/com/springboot/jihyun_blog/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.jihyun_blog.repository;
-
-import com.springboot.jihyun_blog.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
